0108b1251a98160b9310c95be0758b51496462d6
ossamabakir1986-cell <ossama.bakir1986@gmail.com>
2026-09-07 14:33:13 +0400

    Load v3.3.41 filter All button fix in Agent

--- js/agent_inline_2.js
+++ js/agent_inline_2.js
@@ -1,12 +1,11 @@
-
   setTimeout(function(){
     if(window.addCrispMasterPlanLabels) addCrispMasterPlanLabels();
   }, 500);
 
   setTimeout(function(){
-    if(document.querySelector('script[src*="v3340_stable_filter_tabs.js"]')) return;
+    if(document.querySelector('script[src*="v3341_stable_filter_tabs_all_button_fix.js"]')) return;
     var s=document.createElement('script');
-    s.src='js/v3340_stable_filter_tabs.js?v=3340';
+    s.src='js/v3341_stable_filter_tabs_all_button_fix.js?v=3341';
     s.async=false;
     document.body.appendChild(s);
   }, 1200);
