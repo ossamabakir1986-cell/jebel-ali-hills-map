7cd93c7d55ae7552a35d4502299f45bf3e6b8c9c
ossamabakir1986-cell <ossama.bakir1986@gmail.com>
2026-09-10 14:27:17 +0400

    Load v3.3.42 data update in agent

--- js/agent_inline_2.js
+++ js/agent_inline_2.js
@@ -3,9 +3,17 @@
   }, 500);
 
   setTimeout(function(){
+    if(document.querySelector('script[src*="v3342_data_update_2026_09_10.js"]')) return;
+    var s=document.createElement('script');
+    s.src='js/v3342_data_update_2026_09_10.js?v=3342';
+    s.async=false;
+    document.body.appendChild(s);
+  }, 900);
+
+  setTimeout(function(){
     if(document.querySelector('script[src*="v3341_stable_filter_tabs_all_button_fix.js"]')) return;
     var s=document.createElement('script');
     s.src='js/v3341_stable_filter_tabs_all_button_fix.js?v=3341';
     s.async=false;
     document.body.appendChild(s);
-  }, 1200);
+  }, 1300);
