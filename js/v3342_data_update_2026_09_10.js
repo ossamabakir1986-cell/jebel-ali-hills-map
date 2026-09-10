9388f298810ab163aa8b38d36d86cbd0d161dbfd
ossamabakir1986-cell <ossama.bakir1986@gmail.com>
2026-09-10 14:26:56 +0400

    Add v3.3.42 data update patch

---
+++ js/v3342_data_update_2026_09_10.js
@@ -0,0 +1,124 @@
+// Hayat GIS v3.3.42 - Data Update 2026-09-10
+// Applies latest exported map data as a small patch on top of v3.3.41.
+(function(){
+  'use strict';
+  if(window.__HAYAT_V3342_DATA_UPDATE_20260910_LOADED) return;
+  window.__HAYAT_V3342_DATA_UPDATE_20260910_LOADED = true;
+
+  var PATCH = {"updatedAt":"2026-09-10T10:17:59.890Z","recordCount":495,"paCount":2963,"updated":{"5130399":{"lastDateUpdated":"23/08/2026","lastUpdated":"23/08/2026"},"5130434":{"lastDateUpdated":"23/08/2026","lastUpdated":"23/08/2026"},"5130457":{"lastDateUpdated":"23/08/2026","lastUpdated":"23/08/2026"},"5130466":{"lastDateUpdated":"27/08/2026","lastUpdated":"27/08/2026"},"5130469":{"lastDateUpdated":"31/08/2026","lastUpdated":"31/08/2026"},"5130612":{"lastDateUpdated":"30/08/2026","lastUpdated":"30/08/2026"},"5130613":{"lastDateUpdated":"30/08/2026","lastUpdated":"30/08/2026"},"5130681":{"gfaAllowedIsManual":false,"lastDateUpdated":"10/09/2026","lastUpdated":"10/09/2026","lat":24.8938889,"lon":54.9810278,"mapsUrl":"https://www.google.com/maps?q=24.8938889,54.9810278","secondAgent":"","secondColor":"","secondCommission":null,"secondCommissionText":"","secondDeposit":null,"secondDepositText":"","secondMobile":"","secondPrice":null,"secondPriceText":"","secondTotal":null,"secondTotalText":""},"5130682":{"gfaAllowedIsManual":false,"lastDateUpdated":"10/09/2026","lastUpdated":"10/09/2026","lon":54.9807778,"mapsUrl":"https://www.google.com/maps?q=24.894,54.9807778","secondAgent":"","secondColor":"","secondCommission":null,"secondCommissionText":"","secondDeposit":null,"secondDepositText":"","secondMobile":"","secondPrice":null,"secondPriceText":"","secondTotal":null,"secondTotalText":""},"5130705":{"gfaAllowedIsManual":false,"lastDateUpdated":"10/09/2026","lastUpdated":"10/09/2026","lat":24.8938611,"mapsUrl":"https://www.google.com/maps?q=24.8938611,54.97925","secondAgent":"","secondColor":"","secondCommission":null,"secondCommissionText":"","secondDeposit":null,"secondDepositText":"","secondMobile":"","secondPrice":null,"secondPriceText":"","secondTotal":null,"secondTotalText":""},"5130719":{"gfaAllowedIsManual":false,"lastDateUpdated":"10/09/2026","lastUpdated":"10/09/2026","secondAgent":"","secondColor":"","secondCommission":null,"secondCommissionText":"","secondDeposit":null,"secondDepositText":"","secondMobile":"","secondPrice":null,"secondPriceText":"","secondTotal":null,"secondTotalText":""},"5130768":{"lastDateUpdated":"27/08/2026","lastUpdated":"27/08/2026"},"5130914":{"lastDateUpdated":"30/08/2026","lastUpdated":"30/08/2026"},"5130988":{"gfaAllowedIsManual":false,"lastDateUpdated":"10/09/2026","lastUpdated":"10/09/2026","secondAgent":"","secondColor":"","secondCommission":null,"secondCommissionText":"","secondDeposit":null,"secondDepositText":"","secondMobile":"","secondPrice":null,"secondPriceText":"","secondTotal":null,"secondTotalText":""},"5131101":{"lastDateUpdated":"27/08/2026","lastUpdated":"27/08/2026"},"5131102":{"lastDateUpdated":"27/08/2026","lastUpdated":"27/08/2026"},"5131306":{"lastDateUpdated":"03/09/2026","lastUpdated":"03/09/2026"},"5131344":{"lastDateUpdated":"03/09/2026","lastUpdated":"03/09/2026"},"5131842":{"gfaAllowedIsManual":false,"lastDateUpdated":"10/09/2026","lastUpdated":"10/09/2026","secondAgent":"","secondColor":"","secondMobile":""},"5131898":{"lastDateUpdated":"27/08/2026","lastUpdated":"27/08/2026"},"5131922":{"agent":"Abdullah Assani","commission":61032.068999999996,"commissionText":"61,032.07","deposit":305160.345,"depositText":"305,160.35","gfaAllowedIsManual":false,"lastDateUpdated":"27/08/2026","lastUpdated":"27/08/2026","mobile":"+971 56 644 7443","price":315,"priceText":"315","secondAgent":"","secondColor":"","secondCommission":null,"secondCommissionText":"","secondDeposit":null,"secondDepositText":"","secondMobile":"","secondPrice":null,"secondPriceText":"","secondTotal":null,"secondTotalText":"","total":3051603.4499999997,"totalText":"3,051,603"},"5132010":{"lastDateUpdated":"27/08/2026","lastUpdated":"27/08/2026"},"5132011":{"lastDateUpdated":"25/08/2026","lastUpdated":"25/08/2026"},"5132149":{"lastDateUpdated":"27/08/2026","lastUpdated":"27/08/2026"},"5132182":{"lastDateUpdated":"27/08/2026","lastUpdated":"27/08/2026"},"5134563":{"lastDateUpdated":"27/08/2026","lastUpdated":"27/08/2026"},"5134648":{"lastDateUpdated":"27/08/2026","lastUpdated":"27/08/2026"},"5135030":{"lastDateUpdated":"27/08/2026","lastUpdated":"27/08/2026"},"5135049":{"lastDateUpdated":"27/08/2026","lastUpdated":"27/08/2026"},"5135052":{"lastDateUpdated":"27/08/2026","lastUpdated":"27/08/2026"},"5135054":{"lastDateUpdated":"27/08/2026","lastUpdated":"27/08/2026"},"5135055":{"lastDateUpdated":"27/08/2026","lastUpdated":"27/08/2026"},"5135097":{"agent":"Mohammad Alsyoufi","commission":62873.89,"commissionText":"62,873.89","deposit":314369.45,"depositText":"314,369.45","gfaAllowedIsManual":false,"lastDateUpdated":"27/08/2026","lastUpdated":"27/08/2026","mobile":"+971 56 468 4059","price":310,"priceText":"310","secondAgent":"","secondColor":"","secondCommission":null,"secondCommissionText":"","secondDeposit":null,"secondDepositText":"","secondMobile":"","secondPrice":null,"secondPriceText":"","secondTotal":null,"secondTotalText":"","total":3143694.5,"totalText":"3,143,695"},"5135293":{"agent":"Hayat Al Masri","commission":63858.263999999996,"commissionText":"63,858.26","deposit":319291.32,"depositText":"319,291.32","gfaAllowedIsManual":false,"lastDateUpdated":"10/09/2026","lastUpdated":"10/09/2026","mobile":"+971 58 517 6888","price":310,"priceText":"310","secondAgent":"","secondCommission":null,"secondCommissionText":"","secondDeposit":null,"secondDepositText":"","secondMobile":"","secondPrice":null,"secondPriceText":"","secondTotal":null,"secondTotalText":"","total":3192913.1999999997,"totalText":"3,192,913"},"5137653":{"lastDateUpdated":"27/08/2026","lastUpdated":"27/08/2026"},"5137682":{"lastDateUpdated":"31/08/2026","lastUpdated":"31/08/2026"},"5137720":{"lastDateUpdated":"27/08/2026","lastUpdated":"27/08/2026"},"5137841":{"lastDateUpdated":"27/08/2026","lastUpdated":"27/08/2026"},"5138175":{"lastDateUpdated":"27/08/2026","lastUpdated":"27/08/2026"},"5138235":{"gfaAllowedIsManual":false,"lastDateUpdated":"10/09/2026","lastUpdated":"10/09/2026","lat":24.8949167,"lon":54.9823056,"mapsUrl":"https://www.google.com/maps?q=24.8949167,54.9823056","masterPlot":"PA11_021","secondAgent":"","secondColor":"","secondMobile":""}},"added":[{"row":614,"coords":"24.8933040, 54.9948792","mapsUrl":"https://www.google.com/maps?q=24.893304,54.9948792","gisPlot":"5130989","masterPlot":"PA14_223","agent":"Malek Al Masri","mobile":"+971 58 580 7750","size":11625.16,"price":340,"color":"Red","type":"Plot","phase":"14","secondAgent":"","secondMobile":"","secondPrice":null,"secondColor":"","comment":"","lat":24.893304,"lon":54.9948792,"sizeText":"11,625.16","priceText":"340","secondPriceText":"","total":3952554.4,"totalText":"3,952,554","deposit":395255.44,"depositText":"395,255.44","commission":79051.088,"commissionText":"79,051.09","secondTotal":null,"secondTotalText":"","secondDeposit":null,"secondDepositText":"","secondCommission":null,"secondCommissionText":"","gfaPct":65,"gfaAllowed":7556.354,"gfaAllowedText":"7,556.35","features":"Single Row","gfa":"G+1","gfaAllowedIsManual":false,"lastUpdated":"10/09/2026","lastDateUpdated":"10/09/2026"},{"row":612,"coords":"24.9027052, 54.9599125","mapsUrl":"https://www.google.com/maps?q=24.9027052,54.9599125","gisPlot":"5132133","masterPlot":"PA7_126","agent":"Hayat Al Masri","mobile":"+971 58 517 6888","size":11858.83,"price":350,"color":"Red","type":"Plot","phase":"7","secondAgent":"","secondMobile":"","secondPrice":null,"secondColor":"","comment":"purchasing date August 2023 , construction limit 2 years from October 2026 As we had got extension","lat":24.9027052,"lon":54.9599125,"sizeText":"11,858.83","priceText":"350","secondPriceText":"","total":4150590.5,"totalText":"4,150,591","deposit":415059.05000000005,"depositText":"415,059.05","commission":83011.81,"commissionText":"83,011.81","secondTotal":null,"secondTotalText":"","secondDeposit":null,"secondDepositText":"","secondCommission":null,"secondCommissionText":"","gfaPct":null,"gfaAllowed":null,"gfaAllowedText":"","features":"Back to Back","gfa":"","gfaAllowedIsManual":false,"lastUpdated":"07/09/2026","lastDateUpdated":"07/09/2026"},{"row":615,"coords":"24.9104065, 54.9662262","mapsUrl":"https://www.google.com/maps?q=24.9104065,54.9662262","gisPlot":"5134981","masterPlot":"PA4_26","agent":"Lama Ismail","mobile":"+971 52 498 8094","size":10000.03,"price":450,"color":"Red","type":"Plot","phase":"4","secondAgent":"","secondMobile":"","secondPrice":null,"secondColor":"","comment":"","lat":24.9104065,"lon":54.9662262,"sizeText":"10,000.03","priceText":"450","secondPriceText":"","total":4500013.5,"totalText":"4,500,014","deposit":450001.35000000003,"depositText":"450,001.35","commission":90000.27,"commissionText":"90,000.27","secondTotal":null,"secondTotalText":"","secondDeposit":null,"secondDepositText":"","secondCommission":null,"secondCommissionText":"","gfaPct":65,"gfaAllowed":6500.0195,"gfaAllowedText":"6,500.02","features":"Single Row","gfa":"G+1","gfaAllowedIsManual":false,"lastUpdated":"10/09/2026","lastDateUpdated":"10/09/2026"},{"row":613,"coords":"24.9103132, 54.9681481","mapsUrl":"https://www.google.com/maps?q=24.9103132,54.9681481","gisPlot":"5135007","masterPlot":"PA4_52","agent":"Issam Al Halabi","mobile":"+971 50 250 2357","size":10000.03,"price":320,"color":"Red","type":"Plot","phase":"4","secondAgent":"","secondMobile":"","secondPrice":null,"secondColor":"","comment":"","lat":24.9103132,"lon":54.9681481,"sizeText":"10,000.03","priceText":"320","secondPriceText":"","total":3200009.6,"totalText":"3,200,010","deposit":320000.96,"depositText":"320,000.96","commission":64000.192,"commissionText":"64,000.19","secondTotal":null,"secondTotalText":"","secondDeposit":null,"secondDepositText":"","secondCommission":null,"secondCommissionText":"","gfaPct":null,"gfaAllowed":null,"gfaAllowedText":"","features":"Back to Back","gfa":"","gfaAllowedIsManual":false,"lastUpdated":"07/09/2026","lastDateUpdated":"07/09/2026"}],"removed":["PA4_26"],"paUpdated":{"PA4_052":{"t":"PA4_052","lat":24.9103132,"lng":54.9681481}},"paAdded":[],"paRemoved":[]};
+
+  function clean(v){ return String(v == null ? '' : v).replace(/\s+/g,' ').trim(); }
+  function keyOf(p){ return clean((p && (p.gisPlot || p.masterPlot)) || ''); }
+
+  function patchArray(arr){
+    if(!Array.isArray(arr)) return false;
+    var byKey = {};
+    for(var i=0;i<arr.length;i++) byKey[keyOf(arr[i])] = i;
+
+    (PATCH.removed || []).forEach(function(k){
+      k = clean(k);
+      if(Object.prototype.hasOwnProperty.call(byKey,k)){
+        arr.splice(byKey[k],1);
+        byKey = {};
+        for(var j=0;j<arr.length;j++) byKey[keyOf(arr[j])] = j;
+      }
+    });
+
+    Object.keys(PATCH.updated || {}).forEach(function(k){
+      var idx = byKey[clean(k)];
+      if(idx == null) return;
+      var d = PATCH.updated[k] || {};
+      Object.keys(d).forEach(function(field){ arr[idx][field] = d[field]; });
+    });
+
+    (PATCH.added || []).forEach(function(rec){
+      var k = keyOf(rec);
+      if(!k) return;
+      if(Object.prototype.hasOwnProperty.call(byKey,k)){
+        var idx = byKey[k];
+        Object.keys(rec).forEach(function(field){ arr[idx][field] = rec[field]; });
+      } else {
+        arr.push(rec);
+        byKey[k] = arr.length - 1;
+      }
+    });
+
+    return true;
+  }
+
+  function patchPALabels(){
+    var labels = window.JAH_PA_LABELS;
+    if(!Array.isArray(labels)) return false;
+    var byT = {};
+    labels.forEach(function(x,i){ byT[clean(x && x.t)] = i; });
+
+    (PATCH.paRemoved || []).forEach(function(t){
+      t=clean(t);
+      if(Object.prototype.hasOwnProperty.call(byT,t)){
+        labels.splice(byT[t],1);
+        byT = {};
+        labels.forEach(function(x,i){ byT[clean(x && x.t)] = i; });
+      }
+    });
+
+    Object.keys(PATCH.paUpdated || {}).forEach(function(t){
+      var rec = PATCH.paUpdated[t];
+      var idx = byT[clean(t)];
+      if(idx == null) return;
+      Object.keys(rec).forEach(function(field){ labels[idx][field] = rec[field]; });
+    });
+
+    (PATCH.paAdded || []).forEach(function(rec){
+      var t=clean(rec && rec.t);
+      if(!t) return;
+      if(Object.prototype.hasOwnProperty.call(byT,t)){
+        var idx=byT[t];
+        Object.keys(rec).forEach(function(field){ labels[idx][field] = rec[field]; });
+      } else {
+        labels.push(rec);
+        byT[t]=labels.length-1;
+      }
+    });
+    return true;
+  }
+
+  function refreshAfterPatch(){
+    try { window.HAYAT_PUBLISHED_UPDATED = PATCH.updatedAt; } catch(_ ){}
+    try { if(typeof window.refreshFilterOptionsFromPoints === 'function') window.refreshFilterOptionsFromPoints(); } catch(_ ){}
+    try { if(typeof window.hayatApplyFiltersNow === 'function') window.hayatApplyFiltersNow(); } catch(_ ){}
+    try { if(typeof window.applyFilters === 'function') window.applyFilters(); } catch(_ ){}
+    try {
+      if(Array.isArray(window.points) && typeof window.addMarkers === 'function') {
+        window.addMarkers(window.points, false);
+      }
+    } catch(_ ){}
+    try { if(typeof window.addCrispMasterPlanLabels === 'function') window.addCrispMasterPlanLabels(); } catch(_ ){}
+    try {
+      window.dispatchEvent(new CustomEvent('hayat:data-updated', {
+        detail: {version:'v3.3.42', updatedAt:PATCH.updatedAt, records:PATCH.recordCount, paLabels:PATCH.paCount}
+      }));
+    } catch(_ ){}
+  }
+
+  var tries = 0;
+  function apply(){
+    tries++;
+    var did = false;
+    try {
+      if(Array.isArray(window.points)) did = patchArray(window.points) || did;
+      if(Array.isArray(window.HAYAT_PUBLISHED_POINTS)) did = patchArray(window.HAYAT_PUBLISHED_POINTS) || did;
+      patchPALabels();
+    } catch(e) {
+      console.warn('Hayat v3.3.42 data patch error', e);
+    }
+
+    if(did){
+      window.__HAYAT_V3342_DATA_UPDATE_20260910_APPLIED = true;
+      refreshAfterPatch();
+    } else if(tries < 30) {
+      setTimeout(apply, 300);
+    }
+  }
+
+  setTimeout(apply, 250);
+})();
