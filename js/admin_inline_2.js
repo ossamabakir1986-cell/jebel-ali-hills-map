83f354a30ae61713de7c4db646e5f2b31192a014
ossamabakir1986-cell <ossama.bakir1986@gmail.com>
2026-09-12 14:17:05 +0400

    Load v3.3.45 close fix on Admin map

--- js/admin_inline_2.js
+++ js/admin_inline_2.js
@@ -19,17 +19,17 @@
   }, 1300);
 
   setTimeout(function(){
-    if(document.querySelector('script[src*="v3343_arabic_update_log_guide.js"]')) return;
-    var s=document.createElement('script');
-    s.src='js/v3343_arabic_update_log_guide.js?v=3343';
-    s.async=false;
-    document.body.appendChild(s);
-  }, 1700);
-
-  setTimeout(function(){
     if(document.querySelector('script[src*="v3344_role_aware_updates_bilingual_guides.js"]')) return;
     var s=document.createElement('script');
     s.src='js/v3344_role_aware_updates_bilingual_guides.js?v=3344';
     s.async=false;
     document.body.appendChild(s);
-  }, 2150);
+  }, 1700);
+
+  setTimeout(function(){
+    if(document.querySelector('script[src*="v3345_update_panel_close_fix.js"]')) return;
+    var s=document.createElement('script');
+    s.src='js/v3345_update_panel_close_fix.js?v=3345';
+    s.async=false;
+    document.body.appendChild(s);
+  }, 2350);
