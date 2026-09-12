3549657be3cc76b1e9b05f4cf82002b0319942cb
ossamabakir1986-cell <ossama.bakir1986@gmail.com>
2026-09-12 19:27:03 +0400

    Load v3.3.53 unified tools on Admin map

--- js/admin_inline_2.js
+++ js/admin_inline_2.js
@@ -19,25 +19,9 @@
   }, 1300);
 
   setTimeout(function(){
-    if(document.querySelector('script[src*="v3350_single_quick_tools_panel.js"]')) return;
+    if(document.querySelector('script[src*="v3353_unified_tools_translation.js"]')) return;
     var s=document.createElement('script');
-    s.src='js/v3350_single_quick_tools_panel.js?v=3350';
+    s.src='js/v3353_unified_tools_translation.js?v=3353';
     s.async=false;
     document.body.appendChild(s);
   }, 1700);
-
-  setTimeout(function(){
-    if(document.querySelector('script[src*="v3351_small_quick_tools_buttons.js"]')) return;
-    var s=document.createElement('script');
-    s.src='js/v3351_small_quick_tools_buttons.js?v=3351';
-    s.async=false;
-    document.body.appendChild(s);
-  }, 2050);
-
-  setTimeout(function(){
-    if(document.querySelector('script[src*="v3352_guide_language_links_fix.js"]')) return;
-    var s=document.createElement('script');
-    s.src='js/v3352_guide_language_links_fix.js?v=3352';
-    s.async=false;
-    document.body.appendChild(s);
-  }, 2300);
