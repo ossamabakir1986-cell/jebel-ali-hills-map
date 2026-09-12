8897f9085f5883df571c2a1eb00b4c01cfd51534
ossamabakir1986-cell <ossama.bakir1986@gmail.com>
2026-09-12 16:43:52 +0400

    Load v3.3.49 What Changed panel fix on Admin map

--- js/admin_inline_2.js
+++ js/admin_inline_2.js
@@ -33,3 +33,11 @@
     s.async=false;
     document.body.appendChild(s);
   }, 2200);
+
+  setTimeout(function(){
+    if(document.querySelector('script[src*="v3349_what_changed_panel_fix.js"]')) return;
+    var s=document.createElement('script');
+    s.src='js/v3349_what_changed_panel_fix.js?v=3349';
+    s.async=false;
+    document.body.appendChild(s);
+  }, 2600);
