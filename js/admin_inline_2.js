8ef62e2b6f5ac92b91fc1f717116729aeab9d0e2
ossamabakir1986-cell <ossama.bakir1986@gmail.com>
2026-09-12 16:36:07 +0400

    Speed up Admin filters by removing slow UI loaders

--- js/admin_inline_2.js
+++ js/admin_inline_2.js
@@ -19,33 +19,17 @@
   }, 1300);
 
   setTimeout(function(){
-    if(document.querySelector('script[src*="v3344_role_aware_updates_bilingual_guides.js"]')) return;
-    var s=document.createElement('script');
-    s.src='js/v3344_role_aware_updates_bilingual_guides.js?v=3344';
-    s.async=false;
-    document.body.appendChild(s);
-  }, 1700);
-
-  setTimeout(function(){
-    if(document.querySelector('script[src*="v3345_update_panel_close_fix.js"]')) return;
-    var s=document.createElement('script');
-    s.src='js/v3345_update_panel_close_fix.js?v=3345';
-    s.async=false;
-    document.body.appendChild(s);
-  }, 2350);
-
-  setTimeout(function(){
-    if(document.querySelector('script[src*="v3346_mobile_tools_visibility.js"]')) return;
-    var s=document.createElement('script');
-    s.src='js/v3346_mobile_tools_visibility.js?v=3346';
-    s.async=false;
-    document.body.appendChild(s);
-  }, 2700);
-
-  setTimeout(function(){
     if(document.querySelector('script[src*="v3347_top_filter_tools_detailed_updates.js"]')) return;
     var s=document.createElement('script');
     s.src='js/v3347_top_filter_tools_detailed_updates.js?v=3347';
     s.async=false;
     document.body.appendChild(s);
-  }, 3200);
+  }, 1700);
+
+  setTimeout(function(){
+    if(document.querySelector('script[src*="v3348_fast_arabic_no_observer.js"]')) return;
+    var s=document.createElement('script');
+    s.src='js/v3348_fast_arabic_no_observer.js?v=3348';
+    s.async=false;
+    document.body.appendChild(s);
+  }, 2200);
